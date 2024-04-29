8b09f741fd523504e1c86f7bca83f1d7e9bf672e
garciagarciaadrian <agarcia@alicebiometrics.com>
2024-04-29 10:51:39 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@
 
 import PackageDescription
 
-let version = "2.3.2"
-let checksum = "1054917f23bd9758971ab8a971947cfd1fffe2b0bf6c18e5373811193e781df6"
+let version = "2.3.0"
+let checksum = "18403b837d93c3950484d01b2373b1d6184a25c0e696cb4491cbec5146055f18"
 
 let package = Package(
     name: "AliceOnboarding",
@@ -15,9 +15,6 @@ let package = Package(
         .library(
             name: "AliceOnboarding",
             targets: ["AliceOnboardingBundle"]),
-    ],
-    dependencies: [
-        .package(url: "https://github.com/airbnb/lottie-ios.git", from: "4.4.3")
     ],
     targets: [
         .target(
