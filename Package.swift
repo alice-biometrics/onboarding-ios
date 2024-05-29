c622ca27680b768745507af005860afc552dca2f
Alice Biometrics <dev@alicebiometrics.com>
2024-05-29 11:26:17 +0000

    Update iOS version to 2.3.4

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let version = "2.3.4"
-let checksum = "2cdd041003c645b7de67e4cf692d2fdfbf82c3629f012ca4b311af6bff163357"
+let checksum = "8988610609051b2337b569a8cf8d51a62734f8cf69671de48c320eb6d11a47c9"
 
 let package = Package(
     name: "AliceOnboarding",
